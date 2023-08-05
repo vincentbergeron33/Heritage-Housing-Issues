f8a48f6999d5583afb42601af8397f4dc5e9941e
Vincent Bergeron <vincentbergeron33@gmail.com>
2023-08-05 16:58:03 +0000

    filled data

---
+++ src/machine_learning/fill_missing_data.py
@@ -0,0 +1,7 @@
+ def ArbitraryNumberImputer(df, colum):
+    """
+    The GarageFinishImputer() replaces missing data in each variable
+    by none or a random value between rfn, unf and fin, determined by the Garage Area value.
+    """
+    for i in len
+  
